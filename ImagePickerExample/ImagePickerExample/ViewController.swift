1e12d0e93ad8366e01e33d5f309241742ad8b84d
Andrey Chernoprudov <dinloq@gmail.com>
2020-01-28 13:54:55 +0500

    add example project

---
+++ ImagePickerExample/ImagePickerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePickerExample
+//
+//  Created by Andrey Chernoprudov on 28.01.2020.
+//  Copyright © 2020 Little Stars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
